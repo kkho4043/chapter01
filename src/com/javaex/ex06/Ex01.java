ff0c0ca8e5ed1923762734823deab822d4a8e0f2
'kkho4043' <'kkho4043@naver.com'>
2020-11-20 14:05:46 +0900

    EX

--- src/com/javaex/ex06/Ex01.java
+++ src/com/javaex/ex06/Ex01.java
@@ -2,6 +2,9 @@ package com.javaex.ex06;
 
 public class Ex01 {
 	public static void main(String[] args) {
-		System.out.println("hi");
+		for(int i=0; i<args.length; i++){
+			System.out.println(i + "¹øÂ° °ª: " + args[i]);
+		}
+
 	}
 }
